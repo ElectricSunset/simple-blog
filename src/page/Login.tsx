2a02ff547750e98465ddff4899f633e1b26ec435
shendy <gunawan.shendy@gmail.com>
2025-06-01 21:28:30 +0700

    add form

--- src/page/Login.tsx
+++ src/page/Login.tsx
@@ -2,7 +2,15 @@ import React, { useState } from 'react';
 import { z } from 'zod';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Form } from 'radix-ui';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { authLogin } from '@/services/authentication';
 import { useDispatch } from 'react-redux';
@@ -10,9 +18,10 @@ import { setToken } from '@/state/authSlice';
 
 const loginSchema = z.object({
   email: z
-    .string({ required_error: 'Email is Required' })
+    .string()
+    .min(1, 'Email is Required')
     .email('Please enter a valid email address'),
-  password: z.string({ required_error: 'Password is Required' }),
+  password: z.string().min(1, 'Password is Required'),
 });
 
 const Login: React.FC = () => {
@@ -31,6 +40,7 @@ const Login: React.FC = () => {
     try {
       const response = await authLogin(data.email, data.password);
       dispatch(setToken(response.token));
+      form.reset();
     } catch (err) {
       console.error('Login failed:', err);
     }
@@ -38,55 +48,69 @@ const Login: React.FC = () => {
 
   return (
     <div className='flex items-center justify-center h-screen w-screen'>
-      <div className='bg-white p-6 border-1 border-neutral-200 rounded-2xl space-y-5 m-12 w-full sm:w-90 '>
+      <div className='bg-white p-6 border-1 border-neutral-200 rounded-2xl space-y-5 m-12 w-90 '>
         <h1>Sign In</h1>
-        <Form.Root className='space-y-5' onSubmit={form.handleSubmit(onSubmit)}>
-          <Form.Field name='email' className='space-y-1 flex flex-col'>
-            <Form.Label className='text-sm font-semibold font-neutral-950'>
-              Email
-            </Form.Label>
-            <Form.Control asChild>
-              <input
-                {...form.register('email')}
-                placeholder='Enter your email'
-                type='email'
-                className='py-2.5 px-4 border-neutral-300 border-1 rounded-2xl text-sm font-regular font-neutral-500'
-              />
-            </Form.Control>
-          </Form.Field>
 
-          <Form.Field name='password' className='space-y-1 flex flex-col'>
-            <Form.Label className='text-sm font-semibold font-neutral-950'>
-              Password
-            </Form.Label>
-            <div className='border-neutral-300 rounded-2xl border-1 w-full flex-between py-2.5 px-4  '>
-              <Form.Control asChild>
-                <input
-                  {...form.register('password')}
-                  type={showPassword ? 'text' : 'password'}
-                  placeholder='Enter your password'
-                  className='w-full text-sm font-regular font-neutral-500'
-                />
-              </Form.Control>
-              <img
-                src='../../Icons/eye-icon.svg'
-                alt='Show password'
-                className='w-4 h-3 shrink-0 cursor-pointer'
-                onMouseDown={() => setShowPassword(true)}
-                onMouseUp={() => setShowPassword(false)}
-                onMouseLeave={() => setShowPassword(false)}
-                onTouchStart={() => setShowPassword(true)}
-                onTouchEnd={() => setShowPassword(false)}
-              />
-            </div>
-          </Form.Field>
+        <Form {...form}>
+          <form className='space-y-5' onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name='email'
+              render={({ field }) => (
+                <FormItem className='space-y-1 flex flex-col'>
+                  <FormLabel className='text-sm font-semibold font-neutral-950'>
+                    Email
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder='Enter your email'
+                      type='email'
+                      className='py-2.5 px-4 border-neutral-300 border-1 rounded-2xl text-sm font-regular font-neutral-500'
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-          <Form.Submit asChild className='w-full'>
-            <Button>Login</Button>
-          </Form.Submit>
-        </Form.Root>
+            <FormField
+              control={form.control}
+              name='password'
+              render={({ field }) => (
+                <FormItem className='space-y-1 flex flex-col'>
+                  <FormLabel className='text-sm font-semibold font-neutral-950'>
+                    Password
+                  </FormLabel>
+                  <FormControl>
+                    <div className='border-neutral-300 rounded-2xl border-1 w-full flex-between py-2.5 px-4'>
+                      <Input
+                        {...field}
+                        placeholder='Enter your password'
+                        type={showPassword ? 'text' : 'password'}
+                        className='w-full text-sm font-regular font-neutral-500'
+                      />
+                      <img
+                        src='../../Icons/eye-icon.svg'
+                        alt='Show password'
+                        className='w-4 h-3 shrink-0 cursor-pointer'
+                        onMouseDown={() => setShowPassword(true)}
+                        onMouseUp={() => setShowPassword(false)}
+                        onMouseLeave={() => setShowPassword(false)}
+                        onTouchStart={() => setShowPassword(true)}
+                        onTouchEnd={() => setShowPassword(false)}
+                      />
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type='submit'>Login</Button>
+          </form>
+        </Form>
         <p className='text-sm font-regular font-neutral-950'>
-          {"Don't have an account?"}{' '}
+          {"Don't have an account?"}
           <a
             href='#'
             className=' font-semibold text-sm font-primary-300 hover:underline'
